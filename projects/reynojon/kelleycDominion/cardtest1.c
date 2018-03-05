4f39d112ea6f6a4faaf8ce6cf0db5639919627ba
reynojon <reynojon@oregonstate.edu>
2018-03-05 01:55:19 -0500

    Create cardtest1.c

---
+++ projects/reynojon/kelleycDominion/cardtest1.c
@@ -0,0 +1,93 @@
+/*
+ * cardtest1.c
+ *
+ * Tests the functionality of the adventurer card
+ * 
+ * Costs verified at http://wiki.dominionstrategy.com/index.php/List_of_cards
+ */
+
+#include "dominion.h"
+#include "dominion_helpers.h"
+#include <stdio.h>
+
+void assertInts(int a, int *count);
+
+int main() {
+    int seed = 1000, numTests = 50;
+    int numPlayers = 2;
+	struct gameState G;
+	int k[10] = {adventurer, embargo, village, minion, mine, cutpurse,
+			sea_hag, tribute, smithy, treasure_map};
+	int testFails = 0; //Will remain zero if all tests pass
+	int tempHandSize, draw1, draw2;
+	int i, currentPlayer;
+	
+	// initialize a game state and player cards
+	initializeGame(numPlayers, k, seed, &G);
+	
+	tempHandSize = numHandCards(&G);
+	
+	
+	printf("\n----Testing Adventurer Card----\n\n");
+
+	for(i = 0; i < numTests; i++)
+	{
+	
+	  /************************************************
+	  * Code for my refactored adventurer
+	  ************************************************/
+	  //Playing the card directly bypasses end of turn actions and any check for the card's existence
+      //playAdventurer(&G, G.whoseTurn);  
+
+	  
+	  /************************************************
+	  * Code for Cody Kelley's refactored adventurer
+	  ************************************************/
+	  int drawnTreasure=0;
+	  int cardDrawn;
+	  int temphand[MAX_HAND];// moved above the if statement
+	  int z = 0;// this is the counter for the temp hand
+	  
+		currentPlayer = whoseTurn(&G);
+		
+		//Top two cards in hand should be newly drawn and also be coins
+		draw2 = G.hand[currentPlayer][G.handCount[currentPlayer]-1];
+		draw1 = G.hand[currentPlayer][G.handCount[currentPlayer]-2];
+		
+		printf("Played Adventurer Card. Top 2 cards in hand should be coins\n");	
+		printf("Testing last card in hand\n");
+		assertInts(draw2, &testFails);
+	
+		printf("Testing next-to-last card in hand\n");
+		assertInts(draw1, &testFails);
+		
+		endTurn(&G);
+	}
+
+	if (testFails == 0)
+	{
+		printf("TESTING COMPLETE. %d TESTS PASSED.\n\n", numTests);
+	}
+	else
+	{
+		printf("TESTING COMPLETE. %d PASSED, %d FAILED\n\n", numTests - testFails, testFails);
+	}
+	
+	return 0;
+}
+
+//Verifies a is either copper, silver, or gold. 
+//Prints a message based on the comparison
+//If a and b are not the same, count is increased by 1
+void assertInts(int a, int *count)
+{
+	if ((a == copper) || (a == silver) || (a == gold))
+	{
+		printf("OK\n\n");
+	}
+	else
+	{
+		printf("TEST FAILED!\n\n");
+		(*count)++;
+	}
+}
