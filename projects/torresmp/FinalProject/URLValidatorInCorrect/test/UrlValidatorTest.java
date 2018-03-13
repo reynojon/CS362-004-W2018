597c8c912deb130ad6c8acdcf2714448ccafc423
pedrotp <pedro@pedrotp.com>
2018-03-13 19:22:01 -0400

    new manual tests

--- projects/torresmp/FinalProject/URLValidatorInCorrect/test/UrlValidatorTest.java
+++ projects/torresmp/FinalProject/URLValidatorInCorrect/test/UrlValidatorTest.java
@@ -16,22 +16,31 @@ public class UrlValidatorTest extends TestCase {
 
    public void testManualTest()
    {
-   //You can use this function to implement your manual testing
+      /* NOTE: I've commented out tests that are failing so they don't interrupt execution*/
+
       UrlValidator urlVal = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
 
+      assertTrue(urlVal.isValid("http://www.google.com"));
       assertTrue(urlVal.isValid("http://google.com"));
       assertTrue(urlVal.isValid("http://ww2w.google.com"));
       assertTrue(urlVal.isValid("http://www.google.com/?q=hello"));
       assertTrue(urlVal.isValid("http://255.255.255.255"));
+      // assertTrue(urlVal.isValid("http://255.255.255.255:0"));
+      // assertTrue(urlVal.isValid("ftp://255.com:80/test1/file?action=view")); // BUG: regexvalidator.java line 120 != -> ==
+      // assertTrue(urlVal.isValid("http://go.au:80/t123"));
+      // assertTrue(urlVal.isValid("h3t://255.com/test1/file"));
+      // assertTrue(urlVal.isValid("http://255.com/test1/file"));
 
-      // assertFalse(urlVal.isValid("htitp://www.google.com")); // regexvalidator.java line 120 != -> ==
-      // assertFalse(urlVal.isValid("http:/www.google.com/"));
+      assertFalse(urlVal.isValid("http:/www.google.com/"));
+      assertFalse(urlVal.isValid("://aaa.:0/test1?action=edit&mode=up"));
+      assertFalse(urlVal.isValid(":///..//file"));
+      // assertFalse(urlVal.isValid("htitp://www.google.com"));
+      // assertFalse(urlVal.isValid("http://")); // BUG: it says http instead of file: in line 318 of urlValidator.java
       // assertFalse(urlVal.isValid("http://www.google."));
       // assertFalse(urlVal.isValid("http://1.2.3.4."));
       // assertFalse(urlVal.isValid("http://.1.2.3.4"));
       // assertFalse(urlVal.isValid("http://256.256.256.256"));
       // assertFalse(urlVal.isValid("http://go.a1a"));
-      assertFalse(urlVal.isValid("http://")); // it says http instead of file: in line 318 of urlValidator.java
 
    }
 
